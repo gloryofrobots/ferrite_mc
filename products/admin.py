65f462eca0fd430dc79c45f906d034229681a6de
gloryofrobots <gloryofrobots@gmail.com>
2013-10-29 21:26:16 +0200

    change template add list_display in products admin

--- products/admin.py
+++ products/admin.py
@@ -2,21 +2,19 @@
 from django.contrib import admin
 # import stuff we need from django
 # import app specific shit
-from django.db import models
-from products.models import Product,ProductImage,ProductTranslation,Series,Type,TypeTranslation,Category,CategoryTranslation
+from products.models import Product, ProductImage, ProductTranslation, Series, Type, TypeTranslation, Category, CategoryTranslation
 from django.conf import settings
-from tinymce import models as tiny_models
-from tinymce.widgets import TinyMCE
+
 
 class ProductTranslationInline(admin.StackedInline):
     model = ProductTranslation
     extra = 1
-    max_num = len(settings.LANGUAGES)-1
+    max_num = len(settings.LANGUAGES) - 1
 
     inline_classes = ('grp-collapse grp-open',)
     fieldsets = (
         (None, {
-            'fields': ['language',]
+            'fields': ['language', ]
         }),
         ('Translation', {
             'fields': ['description'],
@@ -26,6 +24,7 @@ class ProductTranslationInline(admin.StackedInline):
     #formfield_overrides = {
     #    models.TextField: {'widget': TinyMCE},
     #}
+
 
 class ProductImageInline(admin.StackedInline):
     model = ProductImage
@@ -38,47 +37,57 @@ class ProductImageInline(admin.StackedInline):
     #    models.TextField: {'widget': TinyMCE},
     #}
 
-class ProductAdmin(admin.ModelAdmin):
-    inlines = (ProductTranslationInline,ProductImageInline)
-    fields = ['series','type','name','description',
-              'frequency_min','frequency_max','insertion_losses_min','insertion_losses_max',
-              'inverse_losses_min','inverse_losses_max','vswr','temperature_min','temperature_max','input_power']
-    #list_filter = ('frequency_min', 'frequency_max')
 
-    search_fields = ['name']
+class ProductAdmin(admin.ModelAdmin):
+    #def upper_case_name(obj):
+    #    return ("%s %s" % (obj.series.name, obj.name))
+    #
+    #upper_case_name.short_description = 'Name'
+
+    inlines = (ProductTranslationInline, ProductImageInline)
+    fields = ['series', 'type', 'name', 'description',
+              'frequency_min', 'frequency_max', 'insertion_losses_min', 'insertion_losses_max',
+              'inverse_losses_min', 'inverse_losses_max', 'vswr', 'temperature_min', 'temperature_max', 'input_power']
+    list_display = ( 'name', 'series', 'type', 'frequency_min', 'frequency_max')
+    list_filter = ('series', 'type', 'frequency_min', 'frequency_max')
+
+
 class TypeTranslationInline(admin.StackedInline):
     model = TypeTranslation
     extra = 1
-    max_num = len(settings.LANGUAGES)-1
+    max_num = len(settings.LANGUAGES) - 1
     inline_classes = ('grp-collapse grp-open',)
     fieldsets = (
         (None, {
-            'fields': ['language',]
+            'fields': ['language', ]
         }),
         ('Translation', {
-            'fields': ['name','long_name'],
+            'fields': ['name', 'long_name'],
         }),
     )
 
+
 class TypeAdmin(admin.ModelAdmin):
-    fields = ['name','long_name','direct_flow_image','reverse_flow_image']
+    fields = ['name', 'long_name', 'direct_flow_image', 'reverse_flow_image']
     inlines = (TypeTranslationInline,)
+
 
 class CategoryTranslationInline(admin.StackedInline):
     model = CategoryTranslation
     extra = 1
-    max_num = len(settings.LANGUAGES)-1
+    max_num = len(settings.LANGUAGES) - 1
 
     inline_classes = ('grp-collapse grp-open',)
 
     fieldsets = (
         (None, {
-            'fields': ['language',]
+            'fields': ['language', ]
         }),
         ('Translation', {
             'fields': ['name'],
         }),
     )
+
 
 class CategoryAdmin(admin.ModelAdmin):
     fields = ['name']
@@ -88,14 +97,13 @@ class CategoryAdmin(admin.ModelAdmin):
 class SeriesProductInline(admin.StackedInline):
     model = Product
     extra = 1
-    fields = [ 'type', 'name', 'description',
+    fields = ['type', 'name', 'description',
               'frequency_min', 'frequency_max', 'insertion_losses_min', 'insertion_losses_max',
               'inverse_losses_min', 'inverse_losses_max', 'vswr', 'temperature_min', 'temperature_max', 'input_power']
 
 
-
 class SeriesAdmin(admin.ModelAdmin):
-    fields = ['name','category']
+    fields = ['name', 'category']
     inlines = (SeriesProductInline,)
 
 # register with CMS
